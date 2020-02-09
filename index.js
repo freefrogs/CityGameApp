2147d08c91ae652f00ae9d200f80959bf83c3930
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-02-09 18:30:58 +0100

    Connect to data base

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const basicDebug = require('debug')('app:startup');
+const dbDebug = require('debug')('app:db');
+const morgan = require('morgan');
 
 const app = express();
+
+const connectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_DATABASE_ADDRESS}/${process.env.DB_DATABASE_NAME}`;
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+  .then(() => dbDebug('Connected to MongoDB...'))
+  .catch((err) => {
+    dbDebug('Could not connect to MongoDB.', err.message);
+  });
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  basicDebug('Morgan enabled...');
+}
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/favicon.ico', express.static('images/favicon.ico'));
+app.use(express.urlencoded({
+  extended: true,
+}));
 
 app.get('/', (req, res) => {
   res.render('main');

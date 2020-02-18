814234d69a97f4fa036cad24d7b14b2c7cbb566d
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-02-18 19:37:29 +0100

    Add style for game section

--- routes/game.js
+++ routes/game.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 //Game view 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   res.render('game');
 });
+
+//Add points
+router.post('/:id', (req, res) => {
+  const codes = ['aaa', 'bbb', 'ccc', 'ddd', 'eee'];
+  console.log(req.body.code);
+  res.redirect('/game');
+})
 
 module.exports = router;

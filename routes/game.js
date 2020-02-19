0d746ef3c451d50f4ff275868aa41d9ba1f47d86
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2020-02-19 22:12:52 +0100

    Add game view update reference to team powers

--- routes/game.js
+++ routes/game.js
@@ -5,7 +5,20 @@ const { checkAuthentication } = require('../middleware/auth');
 
 //Game view 
 router.get('/:id', checkAuthentication, async (req, res) => {
-  res.render('game');
+  const user_id = res.locals.team._id;
+  const team = await Team.findById(user_id);
+  const power1 = team.powers.includes('power1');
+  const power2 = team.powers.includes('power2');
+  const power3 = team.powers.includes('power3');
+  const power4 = team.powers.includes('power4');
+  const power5 = team.powers.includes('power5');
+  res.render('game', {
+    power1: power1,
+    power2: power2,
+    power3: power3,
+    power4: power4,
+    power5: power5,
+  });
 });
 
 //Add points
@@ -14,35 +27,35 @@ router.post('/:id', checkAuthentication, async (req, res) => {
   const { code, power } = req.body;
 
   const team = await Team.findById(user_id);
-  const ptk = team.points + 5
+  const pkt = team.points + 5
 
   if (code === 'aaa' && power ==='task1') {
     team.set ({
-      points: ptk,
+      points: pkt,
     });
     team.powers.push('power1')
     await team.save();
   } else if (code === 'bbb' && power ==='task2') {
     team.set ({
-      points: ptk,
+      points: pkt,
     });
     team.powers.push('power2')
     await team.save();
   } else if (code === 'ccc' && power ==='task3') {
     team.set ({
-      points: ptk,
+      points: pkt,
     });
     team.powers.push('power3')
     await team.save();
   } else if (code === 'ddd' && power ==='task4') {
     team.set ({
-      points: ptk,
+      points: pkt,
     });
     team.powers.push('power4')
     await team.save();
   } else if (code === 'eee' && power ==='task5') {
     team.set ({
-      points: ptk,
+      points: pkt,
     });
     team.powers.push('power5')
     await team.save();
